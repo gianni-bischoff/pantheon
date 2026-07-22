4193660df7d1db89d100187543bc4a1649b4b85b
Gianni Bischoff <oucori@gmail.com>
2026-07-22 21:19:21 +0200

    fix: remove @EventBusSubscriber from PantheonClient (no @SubscribeEvent methods)

--- src/main/kotlin/gg/wildblood/client/PantheonClient.kt
+++ src/main/kotlin/gg/wildblood/client/PantheonClient.kt
@@ -2,8 +2,6 @@ package gg.wildblood.client
 
 import gg.wildblood.Pantheon
 import net.neoforged.api.distmarker.Dist
-import net.neoforged.bus.api.SubscribeEvent
-import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.common.Mod
 import net.neoforged.neoforge.client.gui.ConfigurationScreen
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory
@@ -17,7 +15,6 @@ import thedarkcolour.kotlinforforge.neoforge.forge.LOADING_CONTEXT
  *   (renderers, key mappings, screen factories, etc.).
  */
 @Mod(value = Pantheon.MODID, dist = [Dist.CLIENT])
-@EventBusSubscriber(modid = Pantheon.MODID, value = [Dist.CLIENT])
 object PantheonClient {
     init {
         LOADING_CONTEXT.activeContainer.registerExtensionPoint(IConfigScreenFactory::class.java) { ->

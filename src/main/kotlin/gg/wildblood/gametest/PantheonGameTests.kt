136f467de508dec1c9c4319cb4a207ea53d239de
Gianni Bischoff <oucori@gmail.com>
2026-07-22 20:54:04 +0200

    test: implement Temple BE mirror and SavedData persistence GameTests

--- src/main/kotlin/gg/wildblood/gametest/PantheonGameTests.kt
+++ src/main/kotlin/gg/wildblood/gametest/PantheonGameTests.kt
@@ -223,11 +223,84 @@ class PantheonGameTests {
 
     @GameTest(template = "pantheon_temple_mirror")
     fun pantheon_temple_mirror(helper: GameTestHelper) {
+        val server = helper.level.server
+        val data = PantheonSavedData.get(server)
+        data.season = null
+        data.factions.clear()
+        val templePos = helper.absolutePos(BlockPos(0, 1, 0))
+        helper.setBlock(BlockPos(0, 1, 0), ModBlocks.TEMPLE.get())
+        val posStr = "${templePos.x} ${templePos.y} ${templePos.z}"
+        val source = server.createCommandSourceStack()
+        val createResult = runCatching {
+            server.commands.getDispatcher().execute("pantheon faction create \"pantheon:test_d\" TestD $posStr", source)
+        }
+        if (createResult.isFailure || createResult.getOrNull() == 0) {
+            helper.fail("faction create failed: ${createResult.exceptionOrNull()?.message}")
+            return
+        }
+        val fakePlayer = FakePlayerFactory.getMinecraft(server.overworld())
+        val factionId = ResourceLocation.parse("pantheon:test_d")
+        data.assignPlayerToFaction(fakePlayer.uuid, factionId)
+        val be = server.overworld().getBlockEntity(templePos) as? gg.wildblood.blockentity.TempleBlockEntity
+        if (be == null) {
+            helper.fail("Temple BE not found")
+            return
+        }
+        val faction = data.factions[factionId]
+        if (faction == null) {
+            helper.fail("Faction missing from SavedData")
+            return
+        }
+        be.syncFrom(faction, data.season)
+        if (be.factionId != "pantheon:test_d") {
+            helper.fail("factionId mismatch: ${be.factionId}")
+            return
+        }
+        if (be.memberCount != 1) {
+            helper.fail("memberCount should be 1, got ${be.memberCount}")
+            return
+        }
         helper.succeed()
     }
 
     @GameTest(template = "pantheon_persistence")
     fun pantheon_persistence(helper: GameTestHelper) {
+        val server = helper.level.server
+        val data = PantheonSavedData.get(server)
+        data.season = null
+        data.factions.clear()
+        data.startSeason(7)
+        val templePos = helper.absolutePos(BlockPos(0, 1, 0))
+        data.ensureFaction(
+            ResourceLocation.parse("pantheon:test_e"),
+            "TestE",
+            templePos,
+        )
+        val tag = net.minecraft.nbt.CompoundTag()
+        val registries = server.registryAccess()
+        data.save(tag, registries)
+        val reloaded = PantheonSavedData.load(tag, registries)
+        if (reloaded.season == null) {
+            helper.fail("Season not reloaded")
+            return
+        }
+        if (reloaded.season!!.phase != SeasonState.SeasonPhase.CREATED) {
+            helper.fail("Phase mismatch: ${reloaded.season!!.phase}")
+            return
+        }
+        val f = reloaded.factions[ResourceLocation.parse("pantheon:test_e")]
+        if (f == null) {
+            helper.fail("Faction not reloaded")
+            return
+        }
+        if (f.displayName != "TestE") {
+            helper.fail("Name mismatch: ${f.displayName}")
+            return
+        }
+        if (f.anchor != templePos) {
+            helper.fail("Anchor mismatch: ${f.anchor}")
+            return
+        }
         helper.succeed()
     }
 }
